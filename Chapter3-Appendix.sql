e429a1388611583fe40c80a4ac0bd02f5ef569cd
tportela1 <tportela@email.arizona.edu>
2020-04-15 18:39:26 -0700

    added branches table

--- Chapter3-Appendix.sql
+++ Chapter3-Appendix.sql
@@ -1,1 +1,10 @@
+-- Create first table BRANCHES
 
+CREATE TABLE Branches (
+   branch_id datatype,
+   branch_type datatype,
+   street datatype,
+   city datatype,
+   zipcode datatype,
+   PRIMARY KEY(branch_id)
+);

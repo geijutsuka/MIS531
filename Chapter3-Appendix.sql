196ac005bb80b47d6f724248f463972739caf0c1
Teresa Portela <tportela@email.arizona.edu>
2020-04-15 19:37:58 -0700

    added employees table

--- Chapter3-Appendix.sql
+++ Chapter3-Appendix.sql
@@ -19,3 +19,22 @@ CREATE TABLE Main_Branches (
    branch_id int FOREIGN KEY REFERENCES Branches(branch_id),
    region_id int FOREIGN KEY REFERENCES Regions(region_id)
 );
+
+-- Create table for employees
+
+CREATE TABLE Employees (
+   empl_id   INT   NOT NULL UNIQUE,
+   empl_fname VARCHAR(20) NOT NULL,
+   empl_minitial CHAR(1),
+   empl_lname VARCHAR(40),
+   empl_dob DATE,
+   empl_street VARCHAR(40),
+   empl_city VARCHAR(20),
+   empl_state CHAR(2),
+   empl_zipcode CHAR(10),
+   ssn VARCHAR(12),
+   empl_email VARCHAR(60),
+   empl_startDate DATE,
+   empl_endDate DATE,
+   PRIMARY KEY(empl_id)
+);

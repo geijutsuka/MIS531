3c8000540a33452bfa92958e3de4a881f91cd27d
Teresa Portela <tportela@email.arizona.edu>
2020-04-15 18:56:56 -0700

    added regions table as well as main branches table

--- Chapter3-Appendix.sql
+++ Chapter3-Appendix.sql
@@ -1,5 +1,4 @@
 -- Create first table BRANCHES with  constraints
-
 CREATE TABLE Branches (
    branch_id   INT   NOT NULL UNIQUE,
    branch_type VARCHAR(60) NOT NULL,
@@ -7,4 +6,16 @@ CREATE TABLE Branches (
    city VARCHAR(60),
    zipcode INT,
    PRIMARY KEY(branch_id)
+);
+
+--Regions has to be created next as Main Branches depends on Regions
+CREATE TABLE Regions (
+   region_id   INT   NOT NULL UNIQUE,
+   PRIMARY KEY(region_id)
+);
+
+-- create Main Branches table that has two foreign keys
+CREATE TABLE Main_Branches (
+   branch_id int FOREIGN KEY REFERENCES Branches(branch_id),
+   region_id int FOREIGN KEY REFERENCES Regions(region_id)
 );

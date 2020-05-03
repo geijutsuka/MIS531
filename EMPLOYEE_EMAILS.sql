8a3c6c44b1c48c2dc978137fefa148111c5d0a15
Rebecca Macaulay <macaulay@email.arizona.edu>
2020-05-02 21:21:05 -0700

    not needed separately

--- EMPLOYEE_EMAILS.sql
+++
@@ -1,6 +0,0 @@
--- Create first table EMPLOYEE_EMAILS with  constraints
-CREATE TABLE EMPLOYEE_EMAILS (
-   emp_id char(4) constraint emp_email_fk REFERENCES EMPLOYEES,
-   emp_email varchar(6),
-   constraint emp_email_pk PRIMARY KEY(emp_id)
-);

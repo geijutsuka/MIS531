74aa7da0aecf5d0ddd2296d3e3a0f7abd84d6374
Rebecca Macaulay <macaulay@email.arizona.edu>
2020-05-02 21:23:16 -0700

    not needed separately

--- SESSIONS.sql
+++
@@ -1,8 +0,0 @@
--- Create table SESSIONS with constraints
-CREATE TABLE SESSIONS (
-   sessionID char(7) constraint session_pk PRIMARY KEY, 
-   start_date date,
-   end_date date,
-   location varchar(255),
-   empID char(9) constraint session_fk REFERENCES EMPLOYEES
-);

0f19147f1b269d42e22386db4e4d617afe2dceaf
Rebecca Macaulay <macaulay@email.arizona.edu>
2020-05-02 21:21:56 -0700

    not needed separately

--- PRODUCTS.sql
+++
@@ -1,7 +0,0 @@
--- Create first table PRODUCTS with  constraints
-CREATE TABLE PRODUCTS (
-   productID char(6) constraint prod_pk PRIMARY KEY,
-   brand varchar(255),
-   product_avg_cost number(5,2)
-   line_number constraint prod_fk REFERENCES PRODUCT_LINES
-);

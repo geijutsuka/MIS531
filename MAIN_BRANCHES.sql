ab111b0959417f0ddc52bd7d8c1eb02aa060b53a
Rebecca Macaulay <macaulay@email.arizona.edu>
2020-05-02 21:21:20 -0700

    not needed separately

--- MAIN_BRANCHES.sql
+++
@@ -1,5 +0,0 @@
--- create Main Branches table that has two foreign keys
-CREATE TABLE MAIN_BRANCHES (
-   branch_id int FOREIGN KEY REFERENCES BRANCHES(branch_id),
-   region_id int FOREIGN KEY REFERENCES REGIONS(region_id)
-);
